61cf9087aa3e000ab576c2192b63e1dc68ac200b
ulianzanardig@gmail.com <ulianzanardig@gmail.com>
2024-09-17 14:40:40 -0300

    Atividade 5

---
+++ atividade5.py
@@ -0,0 +1,16 @@
+"""2) Passagem de Parâmetros e Funções de Alta Ordem
+
+Funções de alta ordem são aquelas que recebem outras funções como parâmetros ou as retornam como resultado. 
+Esse conceito pode ser útil na criação de funções genéricas e reutilizáveis. Implemente uma função chamada 
+aplicar_operacao que recebe dois números e uma função como parâmetros. A função fornecida será aplicada aos dois números 
+passados. Crie também uma função chamada gerar_operacao que, com base em uma string de operação ("soma", "multiplicacao"),
+retorne a função apropriada.
+
+Requisitos:
+
+- A função aplicar_operacao deve receber uma função como parâmetro e aplicá-la aos dois números.
+- A função gerar_operacao deve retornar a função correta com base na operação passada.
+- Teste com operações de soma e multiplicação.
+"""
+
+def aplicar_operacao(num1 , num2,)
