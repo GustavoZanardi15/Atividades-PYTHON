c4911e694da078a28d28b27ceca0816006753be3
GustavoZanardi15 <ulianzanardig@gmail.com>
2024-09-21 21:53:57 -0300

    Finalização da atividade

--- atividade1.py
+++ atividade1.py
@@ -9,13 +9,17 @@ Calcule o valor total em estoque (preço * quantidade) para cada produto e exiba
 """
 
 produtos=(
-            ('Monito', 1.500, 10),
-            ('Placa de video', 3.000, 5),
-            ('Mouse', 250, 50)   
-        )
+        ('Monitor', 1500 ,10),
+        ('Placa de video', 3000 ,5),
+        ('Mouse', 250 ,50)   
+)
 
-produto_mais_caro = max(produtos, key=lambda  produto:produto[0])
+produto_mais_caro = max(produtos, key=lambda  produto: produto[1])
 print(f"O produto mais caro em estoque é {produto_mais_caro[0]}")
 
 
 
+for produto in produtos:
+    nome, valor, quantidade = produto
+    valor_total = valor * quantidade
+    print(f"O  valor total do {nome} é de R${valor_total:.2f}")
