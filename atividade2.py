87a57f431200f6c00e4f9b818978b62e9e49b65e
ulianzanardig@gmail.com <ulianzanardig@gmail.com>
2024-09-25 15:15:59 -0300

    Adição de funções para case sensitive

--- atividade2.py
+++ atividade2.py
@@ -7,3 +7,17 @@ nota é o valor.Permitir que o usuário insira o nome de um aluno para consultar
 Exibir a média das notas dos alunos armazenados."""
 
 
+nota_alunos = {
+               "gustavo" : 100,
+               "alisson" : 78,
+               "renan" : 80 
+               }
+
+aluno = input(f"Informe o nome do aluno que deseja ver a nota:").strip().lower()
+if aluno in nota_alunos:
+    print(f"A nota do aluno {aluno.capitalize()} é {nota_alunos[aluno]}")
+else: 
+    print(f"Aluno não encontrado no banco de dados")
+
+
+    
